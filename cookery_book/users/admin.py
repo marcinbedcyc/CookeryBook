d0edd1308d5d6778ebc222156106c1397b114b9b
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-19 21:18:34 +0100

    Add UserAdditionalInfo model

--- cookery_book/users/admin.py
+++ cookery_book/users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserAdditionalInfo
 
-# Register your models here.
+
+admin.site.register(UserAdditionalInfo)
--- cookery_book/users/models.py
+++ cookery_book/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class UserAdditionalInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_img = models.ImageField(
+        default='default_user_pic.png', upload_to='profile_imgs')
+
+    def __str__(self):
+        return f"{self.user.username}'s additional info"
+
+    def save(self, *args, **kwargs):
+        super().save()
+        maxsize = (256, 256)
+        img = Image.open(self.profile_img.path)
+        if img.height > 256 or img.width > 256:
+            img.thumbnail(maxsize)
+            img.save(self.profile_img.path)

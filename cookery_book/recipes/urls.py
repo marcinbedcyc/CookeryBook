9d39f430f543d27b7acc84ab66f7b0cc27aedcde
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-22 11:40:36 +0100

    Add detail recipe view with template

--- cookery_book/recipes/urls.py
+++ cookery_book/recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import recipe_list, RecipesListView
+from .views import RecipesListView, recipe_detail
 
 
 urlpatterns = [
     path("", RecipesListView.as_view(), name="recipes-list"),
+    path("recipes/", RecipesListView.as_view(), name="recipes-list"),
+    path("recipes/<int:pk>", recipe_detail, name="recipe-detail"),
 ]
--- cookery_book/recipes/views.py
+++ cookery_book/recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Recipe
-from django.views.generic import ListView
+from .models import Recipe, RecipeImage, Ingredient
+from django.views.generic import ListView, DetailView
 
 
 class RecipesListView(ListView):
@@ -9,3 +9,21 @@ class RecipesListView(ListView):
     context_object_name = 'recipes'
     ordering = ['-created_date']
     paginate_by = 3
+
+
+class RecipeDetailView(DetailView):
+    template_name = 'recipes/recipe_detail.html'
+    context_object_name = 'recipe'
+    model = Recipe
+
+
+def recipe_detail(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    images = RecipeImage.objects.filter(recipe=recipe)
+    ingredients = Ingredient.objects.filter(recipe=recipe)
+    context = {
+        'recipe': recipe,
+        'images': images,
+        'ingredients': ingredients
+    }
+    return render(request, template_name='recipes/recipe_detail.html', context=context)

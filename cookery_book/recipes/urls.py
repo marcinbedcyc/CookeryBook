22a540ed895537153c46b1230cf8bf84ca5808f7
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2020-02-24 12:11:30 +0100

    Add route to recipe's categories

--- cookery_book/recipes/urls.py
+++ cookery_book/recipes/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import RecipesListView, recipe_detail
+from . import views
 
 
 urlpatterns = [
-    path("", RecipesListView.as_view(), name="recipes-list"),
-    path("recipes/", RecipesListView.as_view(), name="recipes-list"),
-    path("recipes/<int:pk>", recipe_detail, name="recipe-detail"),
+    path("", views.RecipesListView.as_view(), name="recipes-list"),
+    path("recipes/", views.RecipesListView.as_view(), name="recipes-list"),
+    path("soups/", views.SoupsListView.as_view(), name="soups-list"),
+    path("desserts/", views.DessertsListView.as_view(), name="desserts-list"),
+    path("snacks/", views.SnacksListView.as_view(), name="snacks-list"),
+    path("main_courses/", views.MainCoursesListView.as_view(), name="main-courses-list"),
+    path("drinks/", views.DrinksListView.as_view(), name="drinks-list"),
+    path("breakfast/", views.BreakfastListView.as_view(), name="breakfast-list"),
+    path("salads/", views.SaladsListView.as_view(), name="salads-list"),
+    path("appetizers/", views.AppetizersListView.as_view(), name="appetizers-list"),
+    path("preserves/", views.PreservesListView.as_view(), name="preserves-list"),
+    path("recipes/<int:pk>", views.recipe_detail, name="recipe-detail"),
 ]
--- cookery_book/recipes/views.py
+++ cookery_book/recipes/views.py
@@ -3,18 +3,18 @@ from .models import Recipe, RecipeImage, Ingredient
 from django.views.generic import ListView, DetailView
 
 
-class RecipesListView(ListView):
+PAGINATE_BY = 3
+
+
+class BaseRecipeListView(ListView):
     model = Recipe
     template_name = 'recipes/recipes_list.html'
     context_object_name = 'recipes'
+    paginate_by = PAGINATE_BY
+
+
+class RecipesListView(BaseRecipeListView):
     ordering = ['-created_date']
-    paginate_by = 3
-
-
-class RecipeDetailView(DetailView):
-    template_name = 'recipes/recipe_detail.html'
-    context_object_name = 'recipe'
-    model = Recipe
 
 
 def recipe_detail(request, pk):
@@ -27,3 +27,48 @@ def recipe_detail(request, pk):
         'ingredients': ingredients
     }
     return render(request, template_name='recipes/recipe_detail.html', context=context)
+
+
+class SnacksListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.SNACK).order_by('-created_date')
+
+
+class SoupsListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.SOUP).order_by('-created_date')
+
+
+class DessertsListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.DESSERT).order_by('-created_date')
+
+
+class MainCoursesListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.MAIN_COURSE).order_by('-created_date')
+
+
+class AppetizersListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.APPETIZER).order_by('-created_date')
+
+
+class DrinksListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.DRINK).order_by('-created_date')
+
+
+class BreakfastListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.BREAKFAST).order_by('-created_date')
+
+
+class SaladsListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.SALADS).order_by('-created_date')
+
+
+class PreservesListView(BaseRecipeListView):
+    def get_queryset(self):
+        return Recipe.objects.filter(dish_kind=Recipe.PRESERVES).order_by('-created_date')
